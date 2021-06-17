053803272104a7fa118dcef2ceeb0cc9f8524406
Volsunga-Saga1260 <longinus1226@gmail.com>
2021-06-17 08:37:02 +0000

    viewsの反映

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,13 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :fast_name, length: { minimum: 2, maximum: 20 }
+  validates :second_name, length: { minimum: 2, maximum: 20 }
+  validates :kana_fast_name, length: { minimum: 2, maximum: 20 }
+  validates :kana_second_name, length: { minimum: 2, maximum: 20 }
+  validates :second_name, length: { minimum: 2, maximum: 20 }
+  validates :second_name, length: { minimum: 2, maximum: 20 }
+  
+  validates :introduction, length: { maximum: 50 }
 end
