7ff91bb2c581081a9211cab9a2c51ddab75e7e4a
EC2 Default User <ec2-user@ip-172-31-78-7.ec2.internal>
2021-06-17 08:11:05 +0000

    [add]ヘッダーフッターの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  protected
+  def after_sign_path_for(resource)
+    case resource
+    when Customer
+      homes_path
+    when Admin
+      admin_homes_path
+    end
+  end
+
 end
