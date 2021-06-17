7a01653600aff1a38937235430a801e7aad8d244
Volsunga-Saga1260 <longinus1226@gmail.com>
2021-06-17 12:32:03 +0000

    ログイン画面完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:fast_name, :second_name, :kana_fast_name, :kana_second_name, :postal_code, :address, :telephone_number, ])
+  end
+
 end
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,6 @@
 class CustomersController < ApplicationController
+  
+  
+
+
 end
