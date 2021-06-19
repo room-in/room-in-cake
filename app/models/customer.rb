3c1e7a7c3ef26a277e2c31c87833007f7b0a119b
Volsunga-Saga1260 <longinus1226@gmail.com>
2021-06-19 12:50:02 +0000

    add顧客詳細（マイページ）&顧客編集画面完成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,8 +8,6 @@ class Customer < ApplicationRecord
   validates :second_name, length: { minimum: 2, maximum: 20 }
   validates :kana_fast_name, length: { minimum: 2, maximum: 20 }
   validates :kana_second_name, length: { minimum: 2, maximum: 20 }
-  validates :second_name, length: { minimum: 2, maximum: 20 }
-  validates :second_name, length: { minimum: 2, maximum: 20 }
   validates :postal_code, length: { minimum: 7, maximum: 7 }
   validates :address, length: { minimum: 5, maximum: 50 }
   validates :telephone_number, length: { minimum: 11, maximum: 12 }  
