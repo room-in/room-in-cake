c6bdac578d7c534ee996915d8b6fad4c19016f21
MasakoIdei <tarte.groseilles@gmail.com>
2021-06-19 04:26:39 +0000

    [add]edit,show画面の直し、ストロングパラメーター記述

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,5 +8,16 @@ class Admin::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
   def edit
+    @customer = Customer.find(params[:id])
+  end
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:fast_name, :second_name, :kana_fast_name, :kana_second_name, :postal_code, :address, :telephone_number, :email, :is_deleted)
   end
 end
