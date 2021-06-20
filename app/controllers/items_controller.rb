790afeb8ccedea3d4e3014f80171d96f899eeb67
EC2 Default User <ec2-user@ip-172-31-38-6.ec2.internal>
2021-06-20 07:11:57 +0000

    カート内

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,10 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+      @index = "商品"
+  end
+  def show
+    @item = Item.find(params[:id])
+    @cart_items = CartItem.new
+  end
 end
