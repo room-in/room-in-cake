197f983d6d27f540e7f5baddf2e5a0569a5557a8
Souta112 <gameshe75@gmail.com>
2021-06-20 07:20:56 +0000

    商品一覧、編集追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  attachment :image
+  validates :price, presence: true
+  belongs_to :item_genre
+  validates :name, presence: true, uniqueness: true
+  validates :text, presence: true
+  validates :genre, presence: true
+  validates :sub_price, presence: true
+  validates :status, inclusion: { in: [true, false] }
 end
