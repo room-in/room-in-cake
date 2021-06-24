ac01f3163ea27de63518a7a4341cc03d9cb1f322
MasakoIdei <tarte.groseilles@gmail.com>
2021-06-24 03:10:52 +0000

    [add]バリデーション追加（空白制限、カタカナ制限）

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,18 +4,22 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :fast_name, length: { minimum: 2, maximum: 20 }
-  validates :second_name, length: { minimum: 2, maximum: 20 }
-  validates :kana_fast_name, length: { minimum: 2, maximum: 20 }
-  validates :kana_second_name, length: { minimum: 2, maximum: 20 }
-  validates :postal_code, length: { minimum: 7, maximum: 7 }
-  validates :address, length: { minimum: 5, maximum: 50 }
-  validates :telephone_number, length: { minimum: 11, maximum: 12 }
+# 登録時のバリデーション、文字数、空白、入力文字の制限
+  validates :fast_name, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :second_name, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :kana_fast_name, presence: true, length: { minimum: 2, maximum: 20 },
+  format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,message: "フリガナは、全角カタカナのみで入力して下さい"}
+  validates :kana_second_name,presence: true, length: { minimum: 2, maximum: 20 },
+  format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,message: "フリガナはシュウセイ全角カタカナのみで入力して下さい"}
+  validates :postal_code,presence: true , length: { minimum: 7, maximum: 7 }
+  validates :address, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :telephone_number, presence: true, length: { minimum: 10, maximum: 12 }
 
+#アソシエーション
   has_many :adresses, dependent: :destroy
   has_many :cart_items, dependent: :destroy
-  
-  
+
+  # 退会機能
   def active_for_authentication?
     super && (self.is_deleted == false)
   end
