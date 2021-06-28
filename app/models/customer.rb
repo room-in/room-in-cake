8ef4406062a1b9b3de4c4a53181c00314bcaa255
EC2 Default User <ec2-user@ip-172-31-38-6.ec2.internal>
2021-06-28 07:56:43 +0000

    修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,12 +5,12 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 # 登録時のバリデーション、文字数、空白、入力文字の制限
-  validates :fast_name, presence: true, length: { minimum: 2, maximum: 20 }
-  validates :second_name, presence: true, length: { minimum: 2, maximum: 20 }
-  validates :kana_fast_name, presence: true, length: { minimum: 2, maximum: 20 },
+  validates :fast_name, presence: true, length: { minimum: 1, maximum: 20 }
+  validates :second_name, presence: true, length: { minimum: 1, maximum: 20 }
+  validates :kana_fast_name, presence: true, length: { minimum: 1, maximum: 20 },
   format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,message: "フリガナは、全角カタカナのみで入力して下さい"}
-  validates :kana_second_name,presence: true, length: { minimum: 2, maximum: 20 },
-  format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,message: "フリガナはシュウセイ全角カタカナのみで入力して下さい"}
+  validates :kana_second_name,presence: true, length: { minimum: 1, maximum: 20 },
+  format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,message: "フリガナは全角カタカナのみで入力して下さい"}
   validates :postal_code,presence: true , length: { minimum: 7, maximum: 7 }
   validates :address, presence: true, length: { minimum: 5, maximum: 50 }
   validates :telephone_number, presence: true, length: { minimum: 10, maximum: 12 }
